4a0bd5476235b33efa35e1c07fd4df0608021685
Gregoire Courtois <courtoisgregoire@gmail.com>
2019-04-02 18:51:54 +0200

    seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -53,31 +53,37 @@ end
 def import_parts_from_sets(request, url = nil)
   key = "3c19aaab1cb4a59b4776e75383e9bc2f"
   page_size = 5000
-  url = "https://rebrickable.com/api/v3/lego/sets/#{request}/parts/?key=#{key}" if url.nil?
+  url = "https://rebrickable.com/api/v3/lego/sets/#{request}/parts/?key=#{key}&page_size=2000" if url.nil?
   response = open(url).read
   result = JSON.parse(response)
   in_box = Box.where(box_sku: request).first
   if result["count"].positive?
     puts "#{in_box.box_sku} - #{in_box.name} - #{result["count"]} parts awaited"
     result["results"].each do |part|
-      p part_number = part["part"]["part_num"]
-      box_part = Part.where(part_sku: part_number).first
-      in_box.parts << box_part unless box_part.nil?
-      puts "#{part["part"]["part_num"]} added"if in_box.save
+      part_number = part["part"]["part_num"]
+      part["quantity"].times do
+        box_part = Part.where(part_sku: part_number).first
+        in_box.parts << box_part unless box_part.nil?
+      end
+      in_box.save
     end
-    p in_box.parts.length
   else
     puts "No parts for this set !!!"
+  end
+  unless result['next'].nil?
+    puts "Moving to another #{request} page"
+    url = result['next']
+    import_element(request, url) unless result['next'].nil?
   end
 end
 
 def add_parts_to_boxes
   Box.all.each do |box|
-    box.parts = []
-    request = box.box_sku
-    import_parts_from_sets(request)
-    p box.parts.length
-    puts "#{box.box_sku} - #{box.name} - #{box.parts.length} parts added"
+    if box.parts.length.positive?
+      request = box.box_sku
+      import_parts_from_sets(request)
+      puts "#{box.box_sku} - #{box.name} - #{box.parts.length} parts added"
+    end
   end
   puts "All sets done"
 end
